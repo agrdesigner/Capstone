afae7cefe4d5536c08dcbecc85b4d89aa942f6b8
agrdesigner <aline.agrdesigner@gmail.com>
2016-12-04 09:40:06 -0800

    Continue styling

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 $(function() {
 
+  // Site-main Carousel
+
   $('.site-main').slick({
     centerMode: true,
     variableWidth: true
   });
+
+  // Sidebar
 
   var sidebar = $('.navigation').sidebar({ side:'right'});
 
   $('.fa-bars').on('click', function(){
     sidebar.trigger('sidebar:toggle');
   });
+
+  // Animations
+
+  // Form Validation
 
 
 });
